8bd22c659e3f5bbc3267525b4ecbfc2f3f5be41c
horacenjoroge <125595726+horacenjoroge@users.noreply.github.com>
2024-05-20 22:45:49 +0300

    removed profile bug

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -24,6 +24,7 @@ export default function Dashboard() {
         {/* Sidebar */}
         <DashSidebar />
       </div>
+     
       {/* profile... */}
       {tab === 'profile' && <DashProfile />}
       {/* posts... */}
